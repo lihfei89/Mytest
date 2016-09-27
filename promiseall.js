81eb54aca3c540dac2b8421573e9baedf8aeb369
ll <jonhjwsosd@163.com>
2016-09-27 11:31:55 +0800

    merge

--- promiseall.js
+++ promiseall.js
@@ -1,63 +1,117 @@
-// function returnPromiseObj(){
 
-// }
 
-function a(isTrue){
-    return new Promise(function(reject,resolve){
-        if (isTrue) {
-            resolve('hello ');
-        }
-        else{
-            reject('Hi ');
-        }
-    }).catch(function(err){
+function a(isTrue) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (isTrue) {
+                resolve('Hello ');
+            }
+            else {
+                reject('Hi ');
+            }
+        }, 500);
+    }).catch(function (err) {
+        console.log(arguments.length+" -----");
         return err;
-    });
+    })
 }
-function b(isTrue){
-    return new Promise(function(reject,resolve){
-        if (isTrue) {
-            resolve('world ');
-        }
-        else{
-            reject('Node ');
-        }
-    }).catch(function(err){
-        return err;
-    });
+function b(isTrue) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (isTrue) {
+                resolve('world ');
+            }
+            else {
+                reject('Node ');
+            }
+        }, 500);
+    })
 }
-function c(isTrue){
-    return new Promise(function(reject,resolve){
-        if (isTrue) {
-            resolve('!!! ');
-        }
-        else{
-            reject('??? ');
-        }
-    }).catch(function(err){
-        return err;
-    });
+function c(isTrue) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (isTrue) {
+                resolve('!!! ');
+            }
+            else {
+                reject('??? ');
+            }
+        }, 500);
+
+    })
 }
 
-// a(true).then(b(true),function(res){
-//     return res;
-// }).then(c(true)).then(function(res){
+
+a(true).then(function (res) {
+    return res;
+}).then(function (res1) {
+    return b(true).then(function (res) {
+        return res;
+    }).then(function (res2) {
+        return res1 + res2;
+    })
+}).then(function (res1) {
+    return c(true).then(function (res) {
+        return res;
+    }).then(function (res2) {
+        return res1 + res2;
+    })
+}).then(function (res3) {
+    console.log(res3);
+})
+
+
+
+a(true).then(function(resA){
+    return resA;
+}).then(function(resA){
+    
+})
+
+console.log('-----------------------');
+
+// a(false).then(function (res) {
 //     console.log(res);
-// });
-Promise.all([a(false),b(true),c(false)]).then(function(res){
+// }, function (rej) {
+//     console.log(rej);
+// })
+// // b(true).then(function (res) {
+//     console.log(res);
+// }, function (rej) {
+//     console.log(rej);
+// })
+// c(true).then(function (res) {
+//     console.log(res);
+// }, function (rej) {
+//     console.log(rej);
+// })
+
+console.log('-----------------------');
+
+Promise.all([a(false), b(true), c(true)]).then(function (res) {
+    console.log('----------1');
     console.log(res);
+}, function (rej) {
+    console.log('---------2');
+    console.log(rej);
 });
 
-Promise.race([a(true),b(false),c(true)]).then(function(res){
+Promise.race([a(false), b(true), c(true)]).then(function (res) {
+    console.log('----------3');
     console.log(res);
-},function(res){
-    console.log(res);
-},function(res){
-    console.log(res);
+}, function (rej) {
+    console.log('---------4');
+    console.log(rej);
 });
 
-console.log('object');
 
 
 /* https://cauu.github.io/2015/10/promise/ */
 /* http://www.oschina.net/translate/service-discovery-in-the-cloud */
+
+
+
+
+
+
+
